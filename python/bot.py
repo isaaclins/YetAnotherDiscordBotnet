efa9820782f48419607cbe9ef7ff9975b002c1b0
Isaac Lins <isaac.lins07@gmail.com>
2024-10-03 14:33:12 +0200

    Remove unnecessary files and code

--- python/bot.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-from discord.ext.commands import Bot
-
-intents = discord.Intents.all()
-intents.members = True
-client = discord.Client(intents=intents)
--- yetanotherdiscordratfrontend/src/index.js
+++
@@ -1,1 +0,0 @@
-import React from 'react';\nimport ReactDOM from 'react-dom';\n\nconst App = () => <h1>Hello World</h1>;\n\nReactDOM.render(<App />, document.getElementById('root'));
