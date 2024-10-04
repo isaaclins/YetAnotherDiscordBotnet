a5b9c866033927eff2cdf616b4ac89aa16b74f8c
Isaac Lins <isaac.lins07@gmail.com>
2024-10-04 10:57:56 +0200

    Refactor: Update form layout and styles in page.tsx

--- yetanotherfrontend/app/python/page.tsx
+++ yetanotherfrontend/app/python/page.tsx
@@ -55,18 +55,18 @@ const Page: React.FC = () => {
     }
 
     return (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className="flex items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} method="POST" className="w-2/3 space-y-6">
-                <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                    <Input {...field} placeholder="Username" />
-                )}
-                />
-                <Button type="submit">Save</Button>
-            </form>
+                <form onSubmit={form.handleSubmit(onSubmit)} method="POST" className="w-2/3 space-y-6 flex flex-col items-center">
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <Input {...field} placeholder="Username" className="w-full sm:w-1/2" />
+                        )}
+                    />
+                    <Button type="submit" className="w-full sm:w-1/2">Save</Button>
+                </form>
             </Form>
         </div>
     );
