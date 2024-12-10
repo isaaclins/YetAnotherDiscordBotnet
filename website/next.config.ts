6b339c316e56cabd90ab5d9158f505b14fa13f31
Isaac Lins <contact@isaaclins.com>
2024-12-10 13:38:36 +0100

    fix(workflow): add CNAME for GitHub Pages deployment and update Next.js configuration for base path and asset prefix

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/yetanotherdiscordbotnet',
+  assetPrefix: '/yetanotherdiscordbotnet/',
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
