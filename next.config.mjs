c244e93954158ea082a29bed29accb098b935841
Isaac Lins <contact@isaaclins.com>
2024-12-10 15:27:21 +0100

    feat: add GitHub Actions workflow for deploying to GitHub Pages; update Next.js config for basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: '/YetAnotherDiscordBotnet',
+    assetPrefix: '/YetAnotherDiscordBotnet/',
+  };
+  
+  module.exports = nextConfig;
