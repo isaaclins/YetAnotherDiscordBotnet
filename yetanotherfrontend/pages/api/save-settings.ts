8896858d3aa69fe1fbd75fcae1b111bd7d10559f
Isaac Lins <contact@isaaclins.com>
2024-12-11 21:08:56 +0100

    feat: add settings.json for bot configuration and update save-settings API path

--- yetanotherfrontend/pages/api/save-settings.ts
+++ yetanotherfrontend/pages/api/save-settings.ts
@@ -12,7 +12,7 @@ export default async function handler(
     try {
       const parsedData = await settingsSchema.parseAsync(req.body);
       const datajson = JSON.stringify(parsedData, null, 2);
-      const dirPath = path.join(process.cwd(), "../python/settings/");
+      const dirPath = path.join(process.cwd(), "../settings/");
       const filePath = path.join(dirPath, "settings.json");
 
       if (!fs.existsSync(dirPath)) {
