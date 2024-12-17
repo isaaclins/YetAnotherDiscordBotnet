44f09f09ae6a7506a4d723418e9eb872bb973ac1
Isaac Lins <contact@isaaclins.com>
2024-12-17 18:47:47 +0100

    added structured audiomanipulation.py module

--- python/components/TODO/AudioManipulation.py
+++ python/components/TODO/AudioManipulation.py
@@ -6,3 +6,35 @@
 # -increase <int>
 # -set <int>
 # ----------------------
+
+def get_code():
+    return """
+    # ADDED AUDIO MANIPULATION MODULE
+    elif message.content.lower().startswith(".lower"):
+        volume = message.content[7:]
+        try:
+            volume = int(volume)
+            if volume < 0:
+                await message.channel.send("Volume cannot be negative")
+            else:
+                # here is the code to lower the volume by the specified amount:
+                
+    elif message.content.lower() == ".increase":
+        volume = message.content[10:]
+        try:
+            volume = int(volume)
+            if volume < 0:
+                await message.channel.send("Volume cannot be negative")
+            else:
+                # Increase the volume by the specified amount
+
+                
+    elif message.content.lower().startswith(".set"):
+        volume = message.content[4:]
+        try:
+            volume = int(volume)
+            if volume < 0:
+                await message.channel.send("Volume cannot be negative")
+            else:
+                # Set the volume to the specified amount
+    """
