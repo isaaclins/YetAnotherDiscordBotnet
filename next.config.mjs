36f4ce07c6e89fda32992593d61c6bec6b80f2f7
Isaac Lins <contact@isaaclins.com>
2024-12-17 16:00:07 +0100

    removed everything

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/YetAnotherDiscordBotnet',
-  assetPrefix: '/YetAnotherDiscordBotnet/',
-  output: 'export',
-};
-
-export default nextConfig;
