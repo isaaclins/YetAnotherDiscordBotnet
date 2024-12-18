323dc14bf3719ec83140bf91ccb3072187264a3a
Isaac Lins <contact@isaaclins.com>
2024-12-18 17:43:11 +0100

    Aktualisiere Kommentar in ClipBoard.py zur Klarstellung der Funktionalität

--- python/components/TODO/ClipBoard.py
+++ python/components/TODO/ClipBoard.py
@@ -1,5 +1,5 @@
 # THIS COMPONENT WILL:
-# contain the command to be able to write down the clipboard and also add stuff to the clipboard in a single line
+# contain the command to be able to get information from the clipboard and also add stuff to the clipboard in a single line
 # ----------------------
 # TODO:
 # -clipboard
