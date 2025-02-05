adee70857bb0efe327288e080e3ee71a898c35cb
Isaac Lins <contact@isaaclins.com>
2025-02-05 13:15:10 +0100

    Add clipboard functionality to retrieve and add content

--- python/components/TODO/ClipBoard.py
+++ python/components/TODO/ClipBoard.py
@@ -4,3 +4,19 @@
 # TODO:
 # -clipboard
 # -clipboard add <string>
+
+def get_code():
+    return """
+elif message.content.lower() == ".clipboard":
+    import pyperclip
+    # Here is the code to get the content of the clipboard
+    await message.channel.send("```diff\n+ Clipboard\n```")
+    await message.channel.send("```diff\n+ " + pyperclip.paste() + "\n```")
+
+elif message.content.lower().startswith(".clipboard add"):
+    import pyperclip
+    # Here is the code to add content to the clipboard
+    content = message.content[15:]
+    pyperclip.copy(content)
+    await message.channel.send("```diff\n+ Added to Clipboard\n```")
+"""
