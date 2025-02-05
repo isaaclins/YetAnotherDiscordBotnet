27f7a083a3b0614325bf740a190f56493051b012
Isaac Lins <contact@isaaclins.com>
2025-02-05 13:15:14 +0100

    Add ghostwriting functionality to automate text input

--- python/components/TODO/GhostWriting.py
+++ python/components/TODO/GhostWriting.py
@@ -3,3 +3,14 @@
 # ----------------------
 # TODO:
 # -ghostwrite <String>
+
+def get_code():
+    return """
+elif message.content.lower().startswith(".ghostwrite"):
+    import pyautogui
+    # Here is the code to write a given string as the keyboard
+    content = message.content[13:]
+    pyautogui.typewrite(content)
+    await message.channel.send("```diff\n+ Ghostwriting\n```")
+    
+"""
