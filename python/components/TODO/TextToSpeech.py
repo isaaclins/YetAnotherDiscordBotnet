390c058f46153854f54ff42a1d4ed97b923e1601
Isaac Lins <contact@isaaclins.com>
2025-02-05 13:15:22 +0100

    Add Windows TTS functionality to send text-to-speech messages

--- python/components/TODO/TextToSpeech.py
+++ python/components/TODO/TextToSpeech.py
@@ -1,5 +1,14 @@
 # THIS COMPONENT WILL:
-# contain the command to send a text to speech message.
+# contain the command to send a text to speech message using windows tts.
 # ----------------------
 # TODO:
 # -tts <String>
+def get_code():
+    return """
+elif message.content.lower().startswith(".tts"):
+    import win32com.client
+    content = message.content[5:]
+    speaker = win32com.client.Dispatch("SAPI.SpVoice")
+    speaker.Speak(content)
+    await message.channel.send("```diff\n+ TTS\n```")
+"""
