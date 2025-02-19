0f5eef6620a1f40771b92535d1430cdb5c8b9842
Isaac Lins <contact@isaaclins.com>
2025-02-19 16:17:39 +0000

    refactor: remove always faulty test case

--- yetanotherfrontend/__test__/faulty.test.js
+++
@@ -1,6 +0,0 @@
-describe('AlwaysfaultyTest', function () {
-    it('should return error in the pipeline', async function () {
-        throw new Error('❌ Test Failed: This test is always faulty');
-    });
-    
-});
