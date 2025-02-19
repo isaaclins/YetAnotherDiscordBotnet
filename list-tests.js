dd9cfa506f05baf6aa3523d02edc6425cf03fc5e
Isaac Lins <contact@isaaclins.com>
2025-02-19 15:26:24 +0000

    refactor: remove unused list-tests.js script

--- list-tests.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const testDir = path.join(__dirname, 'yetanotherfrontend', '__test__');
-const testFiles = fs.readdirSync(testDir).filter(file => file.endsWith('.test.js'));
-
-console.log(`::set-output name=testFiles::${testFiles.join(',')}`);
