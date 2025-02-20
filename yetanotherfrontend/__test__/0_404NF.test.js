7ae50bc036b4c7262278618fa4696cd2ec37f88b
Isaac Lins <contact@isaaclins.com>
2025-02-20 14:47:03 +0000

    test: add issue annotation to 404 response test case

--- yetanotherfrontend/__test__/0_404NF.test.js
+++ yetanotherfrontend/__test__/0_404NF.test.js
@@ -3,7 +3,10 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
 const URL = 'http://localhost:3000/404NF';
-
+/**
+ * @issue test issue
+ * @body Hello world!!!. this is a test code in the test file 0_404NF.test.js to test the 404 response of the server when the URL is not found in the server. 
+ */
 describe('404 Server Response Test', function () {
     this.timeout(10000); // Set global timeout to 10 seconds
     it('should return 404 NOT FOUND', async function () {
