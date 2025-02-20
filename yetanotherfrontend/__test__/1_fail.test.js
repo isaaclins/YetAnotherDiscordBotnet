58c6fe16e12158a167b7294df81dec7c489f1376
Isaac Lins <contact@isaaclins.com>
2025-02-20 14:41:46 +0000

    workflow: improve code-to-issue-and-branch workflow and clean up test annotations

--- yetanotherfrontend/__test__/1_fail.test.js
+++ yetanotherfrontend/__test__/1_fail.test.js
@@ -1,10 +1,6 @@
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
-/**
- * @issue another test issue
- * @body Hello world!!!. this is a test code in the test file 1_fail.test.js to test the failure of the test case.
- */
 describe('Sample Test Suite', () => {
     it('should always fail', () => {
         expect(true).to.be.false;
