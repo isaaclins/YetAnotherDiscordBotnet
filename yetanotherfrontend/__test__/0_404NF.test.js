14f698c51ccf4c68d01cd7df06e21055bee85e29
Isaac Lins <contact@isaaclins.com>
2025-02-20 15:04:34 +0000

    test: remove issue annotation from 404 response test case

--- yetanotherfrontend/__test__/0_404NF.test.js
+++ yetanotherfrontend/__test__/0_404NF.test.js
@@ -3,10 +3,7 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
 const URL = 'http://localhost:3000/404NF';
-/**
- * @issue test issue 3
- * @body Hello world!!!. this is a test code in the test file 0_404NF.test.js to test the 404 response of the server when the URL is not found in the server. 
- */
+
 describe('404 Server Response Test', function () {
     this.timeout(10000); // Set global timeout to 10 seconds
     it('should return 404 NOT FOUND', async function () {
