d199dd059942d41014adc9b8a807763ad3848511
Isaac Lins <contact@isaaclins.com>
2025-02-21 08:52:27 +0000

    test: remove unused issue annotations from response tests

--- yetanotherfrontend/__test__/0_200OK.test.js
+++ yetanotherfrontend/__test__/0_200OK.test.js
@@ -3,10 +3,7 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
 const URL = 'http://localhost:3000';
-/**
- * @issue Take over the world
- * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
- */
+
 describe('200 Server Response Test', function () {
     this.timeout(10000); // Set global timeout to 10 seconds
     it('should return 200 OK in index URL', async function () {
