cd3ac5c71c3b97669cf88ecc5adc2035d24b972f
Isaac Lins <contact@isaaclins.com>
2025-02-21 09:00:07 +0000

    refactor: remove failing test and update tsconfig formatting; set target to ES2017

--- yetanotherfrontend/__test__/1_fail.test.js
+++
@@ -1,8 +0,0 @@
-const { describe, it } = require('mocha');
-const { expect } = require('chai');
-
-describe('Sample Test Suite', () => {
-    it('should always fail', () => {
-        expect(true).to.be.false;
-    });
-});
