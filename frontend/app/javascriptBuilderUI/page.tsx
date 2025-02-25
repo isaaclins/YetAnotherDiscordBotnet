73ad414e092194207db3a7d4fe536cda66d18c65
Isaac Lins <contact@isaaclins.com>
2025-02-25 12:38:04 +0000

    feat: add 'To Be Done' pages for multiple builder UIs

---
+++ frontend/app/javascriptBuilderUI/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const ToBeDonePage: React.FC = () => {
+    return (
+        <div style={{ textAlign: "center", marginTop: "20%" }}>
+            <h1>To Be Done</h1>
+            <p>This page is under construction. Please check back later.</p>
+        </div>
+    );
+};
+
+export default ToBeDonePage;
