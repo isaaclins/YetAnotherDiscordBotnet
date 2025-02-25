636c4fea5c4a41b3f4f10afa7559398f9e628177
Isaac Lins <contact@isaaclins.com>
2025-02-25 12:50:58 +0000

    test: refactor 200 OK tests to use dynamic endpoints

--- frontend/__unit-test__/0_200OK.test.js
+++ frontend/__unit-test__/0_200OK.test.js
@@ -3,6 +3,8 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
 const URL = 'http://localhost:3000';
+const endpoints = ['/', '/pythonBuilderUI', '/goBuilderUI', '/javaBuilderUI', '/bashBuilderUI', '/javascriptBuilderUI', '/rustBuilderUI'];
+
 /**
  * @issue test
  * @body test
@@ -10,24 +12,17 @@ const URL = 'http://localhost:3000';
 
 describe('200 Server Response Test', function () {
     this.timeout(10000); // Set global timeout to 10 seconds
-    it('should return 200 OK in index URL', async function () {
-        try {
-            const response = await axios.get(URL);
-            expect(response.status).to.equal(200);
-            console.log('✅ Test Passed: Server responded with 200 OK');
-        } catch (error) {
-            console.error(`❌ Test Failed: ${error.message}`);
-            throw error;
-        }
-    });
-    it('should return 200 OK in Python URL', async function () {
-        try {
-            const response = await axios.get(URL + "/pythonBuilderUI");
-            expect(response.status).to.equal(200);
-            console.log('✅ Test Passed: Server responded with 200 OK');
-        } catch (error) {
-            console.error(`❌ Test Failed: ${error.message}`);
-            throw error;
-        }
+
+    endpoints.forEach((endpoint) => {
+        it(`should return 200 OK for ${endpoint}`, async function () {
+            try {
+                const response = await axios.get(URL + endpoint);
+                expect(response.status).to.equal(200);
+                console.log(`✅ Test Passed: Server responded with 200 OK for ${endpoint}`);
+            } catch (error) {
+                console.error(`❌ Test Failed for ${endpoint}: ${error.message}`);
+                throw error;
+            }
+        });
     });
 });
